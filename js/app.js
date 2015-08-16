08e2b129bd3f7d7d3d84f338490feae5b9aca200
Pierre BARON <prbaron22@gmail.com>
2015-08-16 19:14:14 +0100

    Upated JS

--- js/app.js
+++ js/app.js
@@ -1,36 +1,3 @@
 window.addEventListener('load', function () {
     FastClick.attach(document.body);
 }, false);
-
-// Navbar and dropdowns
-var toggle    = document.getElementsByClassName('navbar-toggle')[0],
-    collapse  = document.getElementsByClassName('navbar-collapse')[0],
-    dropdowns = document.getElementsByClassName('dropdown');
-
-// Toggle if navbar menu is open or closed
-function toggleMenu() {
-    toggle.classList.toggle('collapsed');
-    collapse.classList.toggle('collapse');
-    collapse.classList.toggle('in');
-}
-
-// Close all dropdown menus
-function closeMenus() {
-    for (var j = 0; j < dropdowns.length; j++) {
-        dropdowns[j].getElementsByClassName('dropdown-toggle')[0].classList.remove('dropdown-open');
-        dropdowns[j].classList.remove('open');
-    }
-}
-
-// Add click handling to dropdowns
-for (var i = 0; i < dropdowns.length; i++) {
-    dropdowns[i].addEventListener('click', function () {
-        if (document.body.clientWidth < 768) {
-            closeMenus();
-            this.getElementsByClassName('dropdown-toggle')[0].classList.toggle('dropdown-open');
-                this.classList.toggle('open');
-        }
-    });
-}
-
-toggle.addEventListener('click', toggleMenu, false);
