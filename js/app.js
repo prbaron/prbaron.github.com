701a093ab93eed36f6605e3fa3f71956363f4d29
prbaron <prbaron22@gmail.com>
2015-08-18 22:45:31 +0100

    Improved font face rendering

--- js/app.js
+++ js/app.js
@@ -1,14 +1,3 @@
 window.addEventListener('load', function () {
     FastClick.attach(document.body);
 }, false);
-
-var observer = new FontFaceObserver('Mir-Medium', {
-    weight : 400
-});
-
-observer.check()
-    .then(function () {
-        document.getElementsByTagName('body')[0].className += ' font-mir-medium-loaded';
-    }, function () {
-        console.error('Mir Medium is not available');
-    });
